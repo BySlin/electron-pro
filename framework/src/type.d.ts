38b0690926516595339acfdfddf51f64df8baef7
郭亮 <2292735470@qq.com>
2023-01-25 18:54:09 +0800

    feat: 优化代码

--- framework/src/type.d.ts
+++ framework/src/type.d.ts
@@ -1,5 +1,8 @@
 interface Window {
   ep: {
+    epWindowName: string;
+    epWindowId: number;
+    epOpenParams: Record<string, any>;
     versions: Record<string, string>;
     ipcRenderer: import('electron').IpcRenderer;
   };
